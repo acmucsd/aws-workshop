9ab8f86093c64b6b65147e8f5f20e016ce03bc82
Angela Hu <angelahu925@gmail.com>
2025-02-10 21:14:54 -0800

    Updated datasource

--- server/data-source.ts
+++ server/data-source.ts
@@ -1,22 +1,23 @@
-import { DataSource } from 'typeorm';
-import { Trip } from './models/tripModel';
-import dotenv from 'dotenv';
+import { DataSource } from "typeorm";
+import { Trip } from "./models/tripModel";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // Create the DataSource instance
 const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [Trip],
-    migrations: [],
-    subscribers: [],
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true,
+  logging: false,
+  entities: [Trip],
 });
 
 export { AppDataSource };
